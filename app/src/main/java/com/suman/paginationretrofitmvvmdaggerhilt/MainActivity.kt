df813fc49a864ef07f105845d692e0bbe8aabb00
susandanne <susandanne@gmail.com>
2022-12-28 17:43:05 +0600

    check comparator only plz

--- app/src/main/java/com/suman/paginationretrofitmvvmdaggerhilt/MainActivity.kt
+++ app/src/main/java/com/suman/paginationretrofitmvvmdaggerhilt/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         binding.recview.adapter=pageadapter
         viewModel=ViewModelProvider(this)[pageviewmodel::class.java]
         viewModel.list.observe(this){
-
+         pageadapter.submitData(lifecycle,it)
         }
     }
 }
